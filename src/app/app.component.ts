b9dafb8f7d3f9e9ecd353e1de38b96d1ecc4b6f1
dcintur <daniele.cintura@gmail.com>
2020-05-17 07:56:20 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'zero-config',
+  templateUrl: 'zero-config.component.html'
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class ZeroConfigComponent {}
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: 'zero-config',
+  templateUrl: 'zero-config.component.html'
 })
-export class HelloComponent  {
-  @Input() name: string;
-}
+export class ZeroConfigComponent {}
